7698f755d8da5c591eff2d16a1079b050e1ff53e
Surcica Silviu <Surciky_silviu@yahoo.com>
2015-07-23 13:30:00 +0300

    Added new functionality

--- mysite/cards/admin.py
+++ mysite/cards/admin.py
@@ -4,7 +4,7 @@ from .models import Card
 class CardAdmin(admin.ModelAdmin):
       fieldsets = [
         (None,               {'fields': ['name']}),
-        ('Date information', {'fields': ['created_at','url','image','media_type'],'classes': ['collapse']}),
+        ('Date information', {'fields': ['created_at','url','image','media_type','owner'],'classes': ['collapse']}),
     ]
       list_filter = ['created_at']
       search_fields = ['name']  	
--- mysite/cards/models.py
+++ mysite/cards/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Card(models.Model):
+	owner=models.ForeignKey(User)
 	url=models.URLField()
 	name=models.CharField(max_length=6)
 	image=models.ImageField()
 	media_type=models.CharField(max_length=32)
 	created_at=models.DateTimeField('created at')
+
 	def __repr__(self):
 		return '<{}-{}>'.format(self.pk,self.name)
 
