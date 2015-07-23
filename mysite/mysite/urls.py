f6bb9152c46639325eda472aafad57df695ba544
Surcica Silviu <Surciky_silviu@yahoo.com>
2015-07-23 14:02:05 +0300

    Added views

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,6 +5,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls', namespace="polls")),
-    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,10 +12,10 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
-      
+
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    response = "You're looking at the results of question {}."
+    return HttpResponse(response.format(question_id))
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse("You're voting on question {}.".format(question_id))
