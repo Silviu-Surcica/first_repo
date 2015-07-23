c4e5658033ec1828390925d6b44bdf2e7ec2881f
Surcica Silviu <Surciky_silviu@yahoo.com>
2015-07-23 15:53:08 +0300

    Added cards functionality

--- mysite/cards/views.py
+++ mysite/cards/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'cards/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last 20 cards."""
+        return Card.objects.order_by('-created_at')[:20]
+
+
+class DetailView(generic.DetailView):
+    model = Card
+    template_name = 'cards/detail.html'
